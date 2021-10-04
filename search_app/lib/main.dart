b2996a8c7c9170431a9000a892ead380fed98af1
hadtthfpt <hadttth1808050@fpt.edu.vn>
2021-10-04 20:43:47 +0700

    giao dien tim kiem

---
+++ search_app/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Search user',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Search User'),
+          actions: [
+            IconButton(
+                onPressed:() {},
+                icon: Icon(Icons.search)
+            ),
+          ],
+        ),
+        body: SafeArea(
+            child: MyHomePage()
+        ),
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Container(
+          child: Text('total'),
+        ),
+        Row(
+          //column coot
+          children: [
+            Column(
+              children: [
+                Text('Name'),
+                Text('id'),
+              ],
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
